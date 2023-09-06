76e6568c5f508a597269d156654c3384a1368e0c
Teemu eemeli Tupola <Teemu.tupola@student.hamk.fi>
2023-09-06 11:25:33 +0300

    Importit ja muuttujat

--- src/LUCKY7.java
+++ src/LUCKY7.java
@@ -1,9 +1,22 @@
-
+import java.util.Random;
+import java.util.Scanner;
 public class LUCKY7 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Random random = new Random();
+		Scanner in = new Scanner(System.in);
+		String vastaus = null;
+		String raha;
+		
+		int nro1;
+		int nro2;
+		int nro3;
+		int rahat;
+		
+		System.out.println("Kuinka suurella summalla haluat pelata?(kirjoita numero)");
+		raha = in.nextLine();
+		rahat = Integer.parseInt(raha);
 	}
 
 }
