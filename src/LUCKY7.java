4824e40da8d531f0d408e3752fa2f9123213c73c
Teemu eemeli Tupola <Teemu.tupola@student.hamk.fi>
2023-09-06 11:26:18 +0300

    Do/while Looppi

--- src/LUCKY7.java
+++ src/LUCKY7.java
@@ -17,6 +17,45 @@ public class LUCKY7 {
 		System.out.println("Kuinka suurella summalla haluat pelata?(kirjoita numero)");
 		raha = in.nextLine();
 		rahat = Integer.parseInt(raha);
+		do {
+			nro1 = random.nextInt(10)+1;
+			nro2 = random.nextInt(10)+1;
+			nro3 = random.nextInt(10)+1;
+			System.out.println("sinulla on nyt " +rahat+ " rahaa");
+			
+			if(rahat > 0 ) {
+			System.out.println("Arvoit seuraavat numerot! " + nro1 +" "+ nro2 +" "+nro3);
+			}
+			else {
+			break;
+			}
+			
+			if(nro1 == 7 && nro2 == 7 && nro3 == 7){
+			System.out.println("Voitit 10 rahaa!");
+			rahat = rahat + 9;
+			}
+			else if(nro1 == 7 && nro2 == 7 || nro1 == 7 && nro3 == 7 || nro2 == 7 && nro3 == 7) {
+				System.out.println("Voitit 3 rahaa!");
+				rahat = rahat+2; 
+				//nro1 == 7 && nro2 == 7 && nro3 == 7)
+			}
+			else if (nro1 == 7 || nro2 == 7 || nro3 == 7) {
+				System.out.println("Voitit 1 rahan!");
+				
+				
+			}
+			
+			else {
+			rahat--;
+			System.out.println("Hävisit pelin");
+			
+			
+			}
+			System.out.println("Sinulla on " + rahat +" Rahaa");
+			System.out.println("haluatko pelata uudestaan?\nLopettaa voit kirjoittamalla N");
+			vastaus = in.nextLine();
+			
+		}  while (rahat == 0 || !vastaus.equals("N"));
 	}
 
 }
